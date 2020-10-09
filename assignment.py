ef99be585ec3fb0a4cb5ae289cdcf621a0251242
JCho24 <johnc415@deltalearns.ca>
2020-10-09 12:58:53 -0700

    .

--- assignment.py
+++ assignment.py
@@ -2,18 +2,31 @@
 
 import math
 
-def tempConversion():
 
+def tempConversion(deg, unit="fc"):
+    if unit == "cf":
+        ans = (deg * (9/5) + 32)
+    
+    elif unit == "fc":
+        ans = ((deg - 32) * (5/9))
+        
+    ans = round(ans,1)
+    return ans
 
-def factorPair():
+def factorPair(number, factor):
+    fpair = number / factor
+    
+    pairlist = [int(factor), int(fpair)]
+    pairlist.sort()
 
-
+    return pairlist
 
 def cosineLaw():
-
-def convertAngle():
-
+    pass
+def toRadians():
+    pass
 def solution():
-
+    pass
 def quadratic():
+    pass    
 
--- task1.py
+++ task1.py
@@ -2,3 +2,5 @@
 
 import assignment
 
+x = assignment.tempConversion(1, unit = "cf")
+print(x)
